e35c28c72d7fc81c063504289e4906633b4afae0
Math7us <mxtheus829melo@gmail.com>
2024-08-29 16:58:27 -0300

    print board

---
+++ chess/chess/src/chess/ChessPiece.java
@@ -0,0 +1,21 @@
+package chess;
+
+import boardgame.Board;
+import boardgame.Piece;
+import chess.enums.Color;
+
+public class ChessPiece extends Piece {
+
+		private Color color;
+		//constructor
+		public ChessPiece(Board board, Color color) {
+			super(board);
+			this.color = color;
+		}
+		//get set
+		public Color getColor() {
+			return color;
+		}
+		
+		
+}
