4e8c791d98125e28b7cfb5f87d7bc8fe15a16108
Math7us <mxtheus829melo@gmail.com>
2024-08-30 15:07:57 -0300

    Chess Exception and chess position

---
+++ chess/chess/src/chess/chessException.java
@@ -0,0 +1,11 @@
+package chess;
+
+public class chessException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	//constructor
+	public chessException(String msg) {
+		super(msg);
+	}
+	
+}
--- chess/chess/src/chess/chessMatch.java
+++ chess/chess/src/chess/chessMatch.java
@@ -1,10 +1,9 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Position;
 import chess.enums.Color;
-import chess.pieces.Rook;
 import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class chessMatch {
 	private Board board;
@@ -24,10 +23,14 @@ public class chessMatch {
 		}
 		return mat;
 	}
+	//methods
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new chessPosition(column, row).toPosition());
+	}
 	//initialize game
 	private void initialSetup() {
-		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
-		board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+		placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 8, new King(board, Color.BLACK));
+		placeNewPiece('e', 1, new King(board, Color.WHITE));
 	}
 }
