3b019e9f274ad2507d4c61f63247e822e243bfde
Math7us <mxtheus829melo@gmail.com>
2024-08-31 10:17:11 -0300

    move and remove pieces

--- chess/chess/src/application/Program.java
+++ chess/chess/src/application/Program.java
@@ -1,15 +1,31 @@
 package application;
 
+import java.util.Scanner;
+
+import chess.ChessPiece;
 import chess.chessMatch;
+import chess.chessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
 		chessMatch chessMatch = new chess.chessMatch();
-		UI.printBoard(chessMatch.getPieces());
 		
-
+		while(true) {
+			UI.printBoard(chessMatch.getPieces());
+			System.out.println();
+			System.out.print("Source: ");
+			chessPosition source = UI.readChessPosition(sc);
+			
+			System.out.println();
+			System.out.print("Target: ");
+			chessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+		}
+		
 	}
 
 }
